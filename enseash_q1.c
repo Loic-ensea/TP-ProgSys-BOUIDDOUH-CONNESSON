526f2a84878adbb1beeee024ce14831f97227421
Faycal-ensea <faycal.bouiddouh@ensea.fr>
2025-12-08 10:34:30 +0100

    Q1: message d'accueil et prompt

--- enseash_q1.c
+++ enseash_q1.c
@@ -4,13 +4,13 @@
 #define BUFSIZE 128
 #define MAX_STR_LEN 1024
 
-char *welcome[] =
+static const char welcome[] =
     "Bienvenue dans le Shell ENSEA.\n"
     "Pour quitter, tapez 'exit'.\n";
-char *prompt[] = "enseash % \n";
+static const char prompt[] = "enseash % \n";
 
 int main(void) {
-    write(STDOUT_FILENO, welcome, strlen(welcome,MAX_STR_LEN));
-    write(STDOUT_FILENO, prompt,  strlen(prompt,MAX_STR_LEN));
+    write(STDOUT_FILENO, welcome, strnlen(welcome,MAX_STR_LEN));
+    write(STDOUT_FILENO, prompt,  strnlen(prompt,MAX_STR_LEN));
     return 0;
 }
