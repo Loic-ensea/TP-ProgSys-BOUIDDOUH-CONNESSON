8dd2889fe361dd7c364c970f79d5b3f653f39ae7
Faycal-ensea <faycal.bouiddouh@ensea.fr>
2025-12-08 10:27:10 +0100

    Q2: lecture de la commande saisie

--- enseash_q2a.c
+++ enseash_q2a.c
@@ -15,7 +15,7 @@ int main(void)
     ssize_t n;
     char buf[BUFSIZE]
 
-    n = read(STDIN_FILENO,buf,BUFSIZE)
+    n = read(STDIN_FILENO,buf,BUFSIZE);
     if (n > 0 && buf[n - 1] == '\n') 
     {
         buf[n - 1] = '\0';
